cd86a72a7107c5ea0a6965c0d4f23b22e5f23e6c
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-02-25 13:43:15 +0100

    keep form states

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -9,7 +9,9 @@
 <?php $nameField='name'; ?>
 <div class="form-group">
     <label for="name">Project name</label>
-    <input name="{{ $nameField }}" type="text" class="form-control @error($nameField) is-invalid @enderror" id="{{ $nameField }}" placeholder="">
+    <input name="{{ $nameField }}" type="text" class="form-control @error($nameField) is-invalid @enderror" id="{{ $nameField }}" placeholder=""
+        value="{{ old($nameField, '') }}"
+    >
 
     @error($nameField)
     <div class="invalid-feedback">
@@ -20,7 +22,7 @@
 
 <div class="form-group">
     <label for="description">Description</label>
-    <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description" rows="3"></textarea>
+    <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description" rows="3">{{ old('description', '')}}</textarea>
 
     @error('description')
     <div class="invalid-feedback">
@@ -31,7 +33,7 @@
 
 <div class="form-group">
     <label for="image_url">Background image URL</label>
-    <input name="image_url" type="text" class="form-control @error('image_url') is-invalid @enderror" id="image_url" placeholder="">
+    <input name="image_url" type="text" class="form-control @error('image_url') is-invalid @enderror" id="image_url" placeholder="" value="{{ old('image_url', '') }}">
 
     @error('image_url')
     <div class="invalid-feedback">
