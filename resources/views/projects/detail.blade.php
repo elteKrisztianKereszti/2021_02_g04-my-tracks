3061086203aca26c04f762415aba7ae7b07166df
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-03-11 13:21:44 +0100

    modify project list page

--- resources/views/projects/detail.blade.php
+++ resources/views/projects/detail.blade.php
@@ -13,33 +13,23 @@
       </form>
       <a href="new-track.html" class="btn btn-primary">Add new track</a>
       <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action" style="background-color: #bbb7b4">
-          <p class="d-flex justify-content-between align-items-center">
-            <span>
-              Track 1
-              <small>track1.wav</small>
-            </span>
-            <span class="badge badge-primary badge-pill">Muted</span>
-          </p>
-          <ul class="list-group list-group-horizontal">
-            <li class="list-group-item p-1">Cras justo odio</li>
-            <li class="list-group-item p-1">Dapibus ac facilisis in</li>
-            <li class="list-group-item p-1">Morbi leo risus</li>
-          </ul>
-        </a>
-
-        <a href="#" class="list-group-item list-group-item-action" style="background-color: #ab7969">
-          <p class="d-flex justify-content-between align-items-center">
-            <span>
-              Track 1
-              <small>track1.wav</small>
-            </span>
-          </p>
-          <ul class="list-group list-group-horizontal">
-            <li class="list-group-item p-1">Cras justo odio</li>
-            <li class="list-group-item p-1">Dapibus ac facilisis in</li>
-            <li class="list-group-item p-1">Morbi leo risus</li>
-          </ul>
-        </a>
+        @foreach ($project->tracks as $track)
+            <a href="#" class="list-group-item list-group-item-action" style="background-color: {{ $track->color }}">
+            <p class="d-flex justify-content-between align-items-center">
+              <span>
+                Trackname: {{ $track->name }}<br/>
+                @if ($track->filename)
+                    <small>Filename: {{ $track->filename }}</small>
+                @endif
+              </span>
+              <span class="badge badge-primary badge-pill">Muted</span>
+            </p>
+            <ul class="list-group list-group-horizontal">
+              <li class="list-group-item p-1">Cras justo odio</li>
+              <li class="list-group-item p-1">Dapibus ac facilisis in</li>
+              <li class="list-group-item p-1">Morbi leo risus</li>
+            </ul>
+          </a>
+        @endforeach
       </div>
 @endsection
