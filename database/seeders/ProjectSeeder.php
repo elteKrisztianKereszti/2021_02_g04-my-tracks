1b0b8da909001e4d88d692c414e90977b16138e1
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-03-18 13:38:49 +0100

    add filter seed for ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Filter;
 use App\Models\Project;
+use App\Models\Track;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,6 +18,19 @@ class ProjectSeeder extends Seeder
     public function run()
     {
         DB::table('projects')->truncate();
-        Project::factory()->count(15)->hasTracks(5)->create();
+
+        $filters = Filter::all();
+
+        Project::factory(3)
+            ->create()
+            ->each(function ($project) use ($filters) {
+                $project->tracks()->createMany(
+                    Track::factory(5)->make()->toArray()
+                )->each(function ($track) use ($filters) {
+                    for ($i = 0; $i < random_int(1, 4); $i++) {
+                        $track->filters()->toggle($filters[random_int(0, 9)]);
+                    }
+                });
+            });
     }
 }
